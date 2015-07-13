29d5289953874a59df827afca1883804124fcfca
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-07-13 18:17:00 +0000

    final stuff

--- UNO.rb
+++ UNO.rb
@@ -13,7 +13,32 @@ class Opponent
     self.hand.each do |card|
       if is_legal(card)
         if is_wild(card)
-          card.color = "red"
+          red = 0
+          yellow = 0
+          green = 0
+          blue = 0
+          self.hand.each do |check_card|
+            if check_card.color == "red"
+              red += 1
+            elsif check_card.color == "yellow"
+              yellow += 1
+            elsif check_card.color == "green"
+              green += 1
+            elsif check_card.color == "blue"
+              blue += 1
+            end
+          end
+          if red > yellow and red > green and red > blue
+            card.color = "red"
+          elsif yellow > red and yellow > green and yellow > blue
+            card.color = "yellow"
+          elsif green > red and green > yellow and yellow > blue
+            card.color = "green"
+          elsif blue > red and blue > green and blue > yellow
+            card.color = "blue"
+          else
+            card.color = "yellow"
+          end
         end
         $deck.live_pile_add(self.hand, card)
         return
