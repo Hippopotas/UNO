525c431b05dd8cc4fb9c1aac74a1e938c60e28fc
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-07-13 16:34:44 +0000

    start AI

--- UNO.rb
+++ UNO.rb
@@ -5,3 +5,24 @@ input = gets.chomp
 loop do
 puts "Your hand is "
 end
+
+class Game
+  @@deck = Deck.new
+  @@currentCard = nil
+  def self.deck
+    @@deck
+  end
+  def self.currentCard
+    @@currentCard
+  end
+  def initialize()
+    self.currentCard = self.deck.draw
+  end
+end
+
+class Opponent
+  attr_accessor :hand
+  def do_move
+    
+  end
+end
